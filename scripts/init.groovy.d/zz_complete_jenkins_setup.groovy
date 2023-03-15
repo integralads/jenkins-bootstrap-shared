2e19e224683be74e6abc8dee9da0bfc4d12dbad9
jnathan-ias <jnathan@integralads.com>
2023-03-14 18:55:13 -0700

    fix env var reference

--- scripts/init.groovy.d/zz_complete_jenkins_setup.groovy
+++ scripts/init.groovy.d/zz_complete_jenkins_setup.groovy
@@ -1,18 +1,22 @@
-import jenkins.model.Jenkins
-
 /**
-  * Function to check if Jenkins should not configure itself and skip.
+  * Check if Jenkins should not configure itself and skip.
   */
+
 Boolean skipConfiguringJenkins() {
-    !(System.env?.environment in ['dev', 'staging']) ||
+    !(System.env?.ENVIRONMENT in ['dev', 'staging']) ||
         (new File("${Jenkins.instance.rootDir}/autoConfigComplete").exists())
 }
+if(skipConfiguringJenkins()) {
+    println 'Skipping Jenkins auto-configuration'
+    return
+}
+
+/**
+  * From this point onward Jenkins should configure itself.
+  */
+import jenkins.model.Jenkins
 
 void setConfigurationComplete() {
-  if(skipConfiguringJenkins()) {
-      println 'Skipping Jenkins auto-configuration'
-      return
-  }
   new File("${Jenkins.instance.rootDir}/autoConfigComplete").withWriter { w -> w << '' }
   println 'Jenkins autoconfiguration complete, writing flag file...'
 }
